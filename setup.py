4701a3718f561609055495fad34076c30058bdbd
stephantul <stephan.tulkens@uantwerpen.be>
2018-02-05 10:10:48 +0100

    Add setup.py for reuse

---
+++ setup.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""Setup file."""
+
+from setuptools import setup
+from setuptools import find_packages
+
+
+setup(name='old20',
+      version='1.0.0',
+      description='Calculate the old20 distance to a lexicon.',
+      author='Stéphan Tulkens',
+      author_email='stephan.tulkens@uantwerpen.be',
+      url='https://github.com/stephantul/old20',
+      license='MIT',
+      packages=find_packages(exclude=['examples']),
+      classifiers=[
+          'Intended Audience :: Developers',
+          'Programming Language :: Python :: 3'],
+      zip_safe=True)
