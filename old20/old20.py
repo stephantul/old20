e266dec1999571c8d8515c7e192b7f71dd895b90
stephantul <stephan.tulkens@uantwerpen.be>
2018-03-14 11:54:09 +0100

    Added subloop function to speed up sampling.

--- old20/old20.py
+++ old20/old20.py
@@ -1,9 +1,10 @@
 """Calculate the OLD score for a given n."""
+import numpy as np
+
 from pyxdameraulevenshtein import damerau_levenshtein_distance
 from functools import partial
 from itertools import combinations
 from tqdm import tqdm
-from collections import defaultdict
 from math import factorial
 
 
@@ -14,7 +15,25 @@ def num_combinations(n, k):
 
 def old_all(words, show_progressbar=True):
     """Calculate the OLD distance over all words."""
-    return old_n(words, len(words), show_progressbar=show_progressbar)
+    return np.mean(old_subloop(words, show_progressbar), 1)
+
+
+def old_subloop(words, show_progressbar):
+    """Calculate the distance from each word to each other word."""
+    old_words = np.zeros((len(words), len(words)))
+
+    # Damerau-Levenshtein distance is symmetric
+    # So we only need to calculate the distance
+    # between each pair once.
+    total = num_combinations(len(words), 2)
+    for a, b in tqdm(combinations(np.arange(len(words)), 2),
+                     total=total,
+                     disable=not show_progressbar):
+        dist = damerau_levenshtein_distance(words[a], words[b])
+        old_words[a, b] = dist
+        old_words[b, a] = dist
+
+    return old_words
 
 
 def old_n(words, n, show_progressbar=True):
@@ -25,7 +44,7 @@ def old_n(words, n, show_progressbar=True):
     ----------
     words : list
         A list of strings, representing all the types in your corpus.
-    n : int
+    n : list of ints
         The number of neighbors to check
 
     Returns
@@ -33,29 +52,19 @@ def old_n(words, n, show_progressbar=True):
     The old score for a given n.
 
     """
-    if n <= 0:
-        raise ValueError("n should be a positive number.")
-    if len(words) <= n:
+    if any([x <= 0 for x in n]):
+        raise ValueError("all values of n should be positive numbers.")
+    if any([len(words) <= x for x in n]):
         raise ValueError("The number of words you have is lower than or equal "
-                         "to the value of your n. Please lower n.")
+                         "to a value of n. Please lower n.")
     if len(set(words)) != len(words):
         raise ValueError("There are duplicates in your dataset. Please remove"
                          " these, as they will make your estimates unreliable")
 
-    old_words = defaultdict(list)
+    old_vals = np.sort(old_subloop(words, show_progressbar), 1)
 
-    # Damerau-Levenshtein distance is symmetric
-    # So we only need to calculate the distance
-    # between each pair once.
-    total = num_combinations(len(words), 2)
-    for a, b in tqdm(combinations(words, 2),
-                     total=total,
-                     disable=not show_progressbar):
-        dist = damerau_levenshtein_distance(a, b)
-        old_words[a].append(dist)
-        old_words[b].append(dist)
-
-    return {k: sum(sorted(v)[:n]) / n for k, v in old_words.items()}
+    for x in n:
+        yield {w: v[1:n+1].mean() for w, v in zip(words, old_vals)}
 
 
-old20 = partial(old_n, n=20)
+old20 = partial(old_n, n=(20,))
