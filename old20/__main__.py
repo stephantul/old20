75e90c6fe53ca396af47c5a122549ad02d3476c9
stephantul <stephan.tulkens@uantwerpen.be>
2018-06-05 14:04:54 +0200

    add main

---
+++ old20/__main__.py
@@ -0,0 +1,59 @@
+"""calculate the representation distance."""
+import argparse
+import os
+import unicodedata
+
+from .old20 import old20
+
+
+def normalize(string):
+    """Normalize, remove accents and other stuff."""
+    s = unicodedata.normalize("NFKD", string).encode('ASCII', 'ignore')
+    return s.decode('utf-8')
+
+
+def write_scores(path, words, scores, overwrite):
+    """Write scores to a file."""
+    if os.path.exists(path) and not overwrite:
+        raise ValueError("File already exists.")
+
+    with open(path, 'w') as f:
+        for word, score in zip(words, scores):
+            f.write("{}\t{}\n".format(word, score))
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description="Calculate the rd20")
+    parser.add_argument("-i", "--input", type=str,
+                        help="The path to the input file.",
+                        required=True)
+    parser.add_argument("-o", "--output", type=str,
+                        help="The path to the output file.",
+                        required=True)
+    parser.add_argument("-n", metavar="n", type=int, required=True)
+    parser.add_argument("--normalize", type=bool, default=False)
+    parser.add_argument("--overwrite",
+                        const=True,
+                        default=False,
+                        action='store_const')
+    parser.add_argument("--sep", type=str, default="\t")
+    parser.add_argument("--loc", type=int, default=0)
+    parser.add_argument("--header",
+                        const=True,
+                        default=False,
+                        action='store_const')
+
+    args = parser.parse_args()
+
+    words = []
+
+    for idx, x in enumerate(open(args.input).readlines()):
+        if idx == 0 and args.header:
+            continue
+        words.append(x.strip().split(args.sep)[args.loc])
+
+    if args.normalize:
+        words = [normalize(x) for x in words]
+    z = old20(words, [args.n])
+    write_scores(args.output, words, z, args.overwrite)
