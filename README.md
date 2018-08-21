6c4e33f861a096a97ade68434881894deb63a2aa
stephantul <stephan.tulkens@uantwerpen.be>
2018-08-21 09:44:19 +0200

    Update, added speed warning

--- README.md
+++ README.md
@@ -3,6 +3,8 @@ Calculate Yarkoni, Baloto & Yap's OLD20.
 
 * **Warning** this implementation of OLD20 uses the Damerau Levenshtein Distance instead of plain Levenshtein distance. This change has a small but non-significant effect in all experiments we ran. Nonetheless, it is important to be aware of the difference.
 
+* **Warning** this code is _slow_ compared to the [leven_c](http://speech.ilsp.gr/iplr/downloads.htm#leven) utility. If you just need OLD20 scores, use that one.
+
 The OLD20 distance is defined as the average Orthographic Levenshtein Distance (OLD) to the 20 closest neighbors for a given word in a given corpus.
 
 If you use the code in this repository, please cite the following paper:
--- old20/old20.py
+++ old20/old20.py
@@ -3,7 +3,7 @@ import numpy as np
 
 from pyxdameraulevenshtein import damerau_levenshtein_distance
 from functools import partial
-from itertools import combinations
+from itertools import product
 from tqdm import tqdm
 from collections import defaultdict
 
@@ -18,22 +18,29 @@ def old_all(words, show_progressbar=True):
     return np.mean(old_subloop(words, show_progressbar), 1)
 
 
-def old_subloop(words,
+def old_subloop(words_a,
+                words_b,
                 show_progressbar,
                 function=damerau_levenshtein_distance,
                 n=20):
-    """Calculate the distance from each word to each other word."""
-    old_words = np.zeros((len(words), n)) + np.inf
-    maxes = np.max(old_words, 1)
+    """Calculate the distance from each word in word_a to each other word_b."""
+    old_words = dict(zip(words_a, np.zeros((len(words_a), n)) + np.inf))
+    maxes = dict(zip(words_a, np.zeros(len(words_a)) + np.inf))
     max_idx = defaultdict(int)
     # Levenshtein distance is symmetric
     # So we only need to calculate the distance
     # between each pair once.
-    total = num_combinations(len(words), 2)
-    for a, b in tqdm(combinations(np.arange(len(words)), 2),
+    total = len(words_a) * len(words_b)
+    done = set()
+    for a, b in tqdm(product(words_a, words_b),
                      total=total,
                      disable=not show_progressbar):
-        dist = function(words[a], words[b])
+        if a == b:
+            continue
+        if set((a, b)) in done:
+            continue
+        done.add(frozenset((a, b)))
+        dist = function(a, b)
         if maxes[a] > dist:
             row = old_words[a]
             row[max_idx[a]] = dist
@@ -47,11 +54,12 @@ def old_subloop(words,
             max_idx[b] = m
             maxes[b] = row[m]
 
-    return old_words
+    return np.array([old_words[x] for x in words_a])
 
 
 def old_n(words,
-          n,
+          reference=None,
+          n=20,
           show_progressbar=True,
           function=damerau_levenshtein_distance):
     """
@@ -81,7 +89,12 @@ def old_n(words,
     if len(set(words)) != len(words):
         raise ValueError("There are duplicates in your dataset. Please remove"
                          " these, as they will make your estimates unreliable")
+
+    if reference is None:
+        reference = words
+
     vals = old_subloop(words,
+                       reference,
                        show_progressbar,
                        function,
                        max(n))
