df0ec462dcb0a3c8a8b365dcef4442463f004441
stephantul <stephan.tulkens@uantwerpen.be>
2018-08-28 12:33:33 +0200

    bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import find_packages
 
 
 setup(name='old20',
-      version='2.0.0',
+      version='2.1.0',
       description='Calculate the old20 distance to a lexicon.',
       author='Stéphan Tulkens',
       author_email='stephan.tulkens@uantwerpen.be',
